cb9f57de6341ae869d9d88649ad7362eed1e8a54
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-21 21:29:36 +0300

    edit add function to upd db.json

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState,useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -17,16 +16,24 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fatch feedback data
   const fetchFeedback = async ()=>{
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
     setFeedback(data)
     setIsLoding(false)
   }
 
 // add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async(newFeedback) => {
+    const response= await fetch('/feedback',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback]);
   };
 //delete feedback
   const deleteFeedback = (id) => {
